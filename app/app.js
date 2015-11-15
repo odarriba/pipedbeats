3eeafb0a8c3995824f21be12afaa02bfda3b49ea
Óscar de Arriba <odarriba@gmail.com>
2015-11-15 17:32:25 +0100

    Added ui-router and basic templates. Navbar selection based on route.

--- app/app.js
+++ app/app.js
@@ -1,4 +1,23 @@
-var app = angular.module("pipedBeats", []);
+'use strict';
+
+var app = angular.module("pipedBeats", ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('start', {
+      url: "/",
+      templateUrl: "views/start/start.html"
+    })
+    .state('about', {
+      url: "/about",
+      templateUrl: "views/about/about.html"
+    });
+});
 
 $("#player-volume .slider").slider({
   min: 0,
