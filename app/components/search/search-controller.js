e237d98623272a1be2d5003013c3bcf91197ae12
Óscar de Arriba <odarriba@gmail.com>
2015-11-18 21:18:56 +0100

    Remove unused libraries. Add some lines to search-controller.

--- app/components/search/search-controller.js
+++ app/components/search/search-controller.js
@@ -2,12 +2,16 @@
 
 angular.module('pipedBeats').controller('searchController', ['$scope', '$state', '$stateParams', 'soundCloud', function($scope, $state, $stateParams, soundCloud) {
   $scope.searchTerms = $stateParams.searchTerms;
+  $scope.results = {};
+  $scope.loading = true;
 
   if ($scope.searchTerms === '') {
     $state.go('start');
   }
 
   soundCloud.get('/tracks', { q: $scope.searchTerms }, function(tracks){
-    console.log(tracks);
+    // Assign results and disable laoding state
+    $scope.results = tracks;
+    $scope.loading = false;
   });
 }]);
