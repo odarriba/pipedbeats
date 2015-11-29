69e43111cc37a374bcddd3ba78ce587d17e55adf
Óscar de Arriba <oscar.dearriba@the-cocktail.com>
2015-11-29 14:56:52 +0100

    Use slider for player progress.

--- app/initializers/ui.js
+++ app/initializers/ui.js
@@ -1,11 +1,19 @@
 'use strict';
 
-angular.module('pipedBeats').run(function(){
-  $("#player-volume .slider").slider({
+angular.module('pipedBeats').run(['$rootScope', function($rootScope){
+  $rootScope.volumeSlider = $("#player-volume .slider").slider({
     min: 0,
     max: 100,
     value: 50,
     step: 1,
     tooltip: 'show'
   });
-});
+
+  $rootScope.progressSlider = $("#player-progress .slider").slider({
+    min: 0,
+    max: 100,
+    value: 50,
+    step: 1,
+    tooltip: 'show'
+  });
+}]);
