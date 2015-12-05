f81623be19de68cf996ce6ea61c5ddc7a798c218
Óscar de Arriba <oscar.dearriba@the-cocktail.com>
2015-12-05 14:07:18 +0100

    Random song selection and some fixes.

--- app/components/player/player-controller.js
+++ app/components/player/player-controller.js
@@ -6,8 +6,9 @@ angular.module('pipedBeats').controller('playerController', ['$scope', 'soundClo
     $scope.playerObject[0].volume = 0.5;
     $scope.playing = false;
 
-    $scope.currentList = {};
+    $scope.sourceList = {};
     $scope.currentTrackIndex = 0;
+    $scope.playList = [];
 
     $scope.currentTime = "00:00";
     $scope.totalTime = "00:00";
@@ -37,15 +38,12 @@ angular.module('pipedBeats').controller('playerController', ['$scope', 'soundClo
 
     // Function to ckeck if the player is ready to play
     $scope.playerReady = function() {
-      return ($scope.currentList.length > 0 && $scope.currentList[$scope.currentTrackIndex] !== undefined);
+      return ($scope.sourceList.length > 0 && $scope.playList[$scope.currentTrackIndex] !== undefined);
     };
 
     // Function to obtain the information about current track
     $scope.getCurrentTrack = function() {
-      if ($scope.currentList[$scope.currentTrackIndex] === undefined) {
-        $scope.currentTrackIndex = 0;
-      }
-      return $scope.currentList[$scope.currentTrackIndex];
+      return $scope.playList[$scope.currentTrackIndex];
     };
 
     // Function to update the track on the player
@@ -67,12 +65,11 @@ angular.module('pipedBeats').controller('playerController', ['$scope', 'soundClo
         var mins = Math.floor(seconds/60),
             secs = Math.floor(seconds%60);
 
+        // Not valid number, return default value
+        if (isNaN(mins) || isNaN(secs)) { return "00:00"; }
+
         if (mins < 10) { mins = "0"+mins; }
         if (secs < 10) { secs = "0"+secs; }
-
-        if (isNaN(mins) || isNaN(secs)) {
-          return "00:00";
-        }
 
         return mins+":"+secs;
       };
@@ -80,13 +77,12 @@ angular.module('pipedBeats').controller('playerController', ['$scope', 'soundClo
       $scope.currentTime = parseTime($scope.playerObject[0].currentTime);
       $scope.totalTime = parseTime($scope.playerObject[0].duration);
 
-      // Update slider max if needed
+      // Update slider time and max (if needed)
+      $scope.progressSlider.slider('setValue', $scope.playerObject[0].currentTime);
+
       if ($scope.progressSlider.slider('getMax') !== $scope.playerObject[0].duration) {
         $scope.progressSlider.slider('setMax', $scope.playerObject[0].duration);
       }
-
-      // Update slider time
-      $scope.progressSlider.slider('setValue', $scope.playerObject[0].currentTime);
     };
 
     // Function to change the volume value
@@ -114,19 +110,50 @@ angular.module('pipedBeats').controller('playerController', ['$scope', 'soundClo
     };
 
     // Button function to go for the next track.
-    // TODO: random selection fo tracks
     $scope.next = function() {
+      var getNextTrack = function() {
+        var possibleTrack = $scope.sourceList[Math.floor(Math.random()*$scope.sourceList.length)];
+
+        // if there is no track yet, return the first coincidence
+        if ($scope.playList.length === 0) {
+          return possibleTrack;
+        }
+
+        // If there are more than 10 songs from the source, aplply the check on 10%
+        if ($scope.sourceList.length >= 10) {
+          // Only check the last X songs, where X is the 10% of the sourceList's length
+          var lastPlayed = $scope.playList.slice(
+            ($scope.playList.length - 1 - Math.ceil($scope.sourceList.lenth*0.1)),
+            ($scope.playList.length - 1) );
+
+          if (lastPlayed.indexOf(possibleTrack) >= 0) {
+            return getNextTrack();
+          }
+        } else {
+          // There are between 1 and 10, check that is not directly repeated
+          if ($scope.sourceList.length > 1) {
+            if ($scope.playList[$scope.playList.length-1] === possibleTrack) {
+              return getNextTrack();
+            }
+          }
+        }
+
+        // If everything goes well or there is only one song, return the match
+        return possibleTrack;
+      };
+
+      // Go to the next song of the playlist
       $scope.currentTrackIndex++;
 
-      if ($scope.currentTrackIndex >= $scope.currentList.length) {
-        $scope.currentTrackIndex = 0;
+      // If we are pointing to a inexistent track,
+      if ($scope.currentTrackIndex === $scope.playList.length) {
+        $scope.playList.push(getNextTrack());
       }
 
       $scope.updatePlayerTrack();
     };
 
     // Button function to go for the next track.
-    // TODO: Use previous played tracks to navigate.
     $scope.back = function() {
       if ($scope.playerObject[0].currentTime > 5 || $scope.currentTrackIndex <= 0) {
         $scope.playerObject[0].currentTime = 0;
@@ -141,14 +168,16 @@ angular.module('pipedBeats').controller('playerController', ['$scope', 'soundClo
     // Event handler to load a new playlist from other controllers
     $scope.$on('player.loadPlaylist', function (event, arg) {
       if (typeof arg !== Object) {
-        $scope.currentList = arg || {};
+        $scope.sourceList = arg || {};
       }
       else {
-        $scope.currentList = {};
+        $scope.sourceList = {};
       }
 
-      $scope.currentTrackIndex = 0;
-      $scope.updatePlayerTrack();
+      $scope.playList = [];
+
+      $scope.currentTrackIndex = -1;
+      $scope.next();
       $scope.play();
     });
   }
