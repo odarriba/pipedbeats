87d629fa1d2657f4efa277d623ff3d94c6a7b8b7
Óscar de Arriba <oscar.dearriba@the-cocktail.com>
2015-12-08 21:02:19 +0100

    Update 2015-12-08T20:02:16.784Z

--- components/search/search-controller.js
+++ components/search/search-controller.js
@@ -4,6 +4,7 @@ angular.module('pipedBeats').controller('searchController', ['$scope', '$rootSco
   function($scope, $rootScope, $state, $stateParams, playerStatus, soundCloud) {
     $scope.searchTerms = $stateParams.q || '';
     $scope.searchGenre = $stateParams.genre || '';
+    $scope.recoverSearch = $stateParams.recoverSearch || false;
     $scope.results = {};
 
     $scope.currentTrackTitle = "";
@@ -13,31 +14,6 @@ angular.module('pipedBeats').controller('searchController', ['$scope', '$rootSco
     $scope.currentCover = "";
     $scope.lastCover = "";
     $scope.lastLastCover = "";
-
-    if ($scope.searchTerms === '' && $scope.searchGenre === '') {
-      $state.go('start');
-    }
-
-    $scope.loading = true;
-
-    var callHash = {};
-
-    if ($scope.searchTerms !== '') { callHash.q = $scope.searchTerms; }
-    if ($scope.searchGenre !== '' && $scope.searchGenre !== 'all') { callHash.genres = $scope.searchGenre; }
-
-    dataLayer.push({
-      "event" : "search",
-      "searchGenre" : $scope.searchGenre,
-      "searchKeywords" : $scope.searchTerms
-    });
-
-    soundCloud.get('/tracks', callHash, function(tracks){
-      // Assign results and disable laoding state
-      $scope.$apply(function () {
-        $scope.loading = false;
-        $rootScope.$broadcast('player.loadPlaylist', tracks);
-      });
-    });
 
     $scope.$on('playerStatus.change', function() {
       $scope.currentTrackTitle = playerStatus.getCurrentTrack().title;
@@ -55,6 +31,51 @@ angular.module('pipedBeats').controller('searchController', ['$scope', '$rootSco
         }
       }
     });
+
+    // If there is no search params, return to the start.
+    if ($scope.searchTerms === '' && $scope.searchGenre === '') {
+      $state.go('start');
+    }
+
+    // Need to do the search or it's a recovery of the latest one?
+    if ($scope.recoverSearch !== true) {
+      $scope.loading = true;
+
+      var callHash = {};
+
+      if ($scope.searchTerms !== '') { callHash.q = $scope.searchTerms; }
+      if ($scope.searchGenre !== '' && $scope.searchGenre !== 'all') { callHash.genres = $scope.searchGenre; }
+
+      // Track the search
+      dataLayer.push({
+        "event" : "search",
+        "searchGenre" : $scope.searchGenre,
+        "searchKeywords" : $scope.searchTerms
+      });
+
+      soundCloud.get('/tracks', callHash, function(tracks){
+        // Assign results and disable laoding state
+        $scope.$apply(function () {
+          $scope.loading = false;
+          $rootScope.$broadcast('player.loadPlaylist', tracks);
+
+          // Store as current search params
+          $rootScope.currentSearch = {
+            searchGenre : $scope.searchGenre,
+            searchTerms : $scope.searchTerms
+          };
+        });
+      });
+    } else {
+      // Track the search recovery
+      dataLayer.push({
+        "event" : "recoverSearch",
+        "searchGenre" : $scope.searchGenre,
+        "searchKeywords" : $scope.searchTerms
+      });
+
+      playerStatus.notifyChange();
+    }
   }
 ])
 .config(['$stateProvider', '$urlMatcherFactoryProvider',
@@ -66,7 +87,8 @@ angular.module('pipedBeats').controller('searchController', ['$scope', '$rootSco
     .state('search', {
       url: "/search/:genre/?q",
       templateUrl: "../views/search/search.html",
-      controller: 'searchController as searchCtrl'
+      controller: 'searchController as searchCtrl',
+      params: {recoverSearch : false}
     });
   }
 ]);
